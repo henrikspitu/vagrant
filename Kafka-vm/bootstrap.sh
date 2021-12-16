c2f3e182f59baf1bc14c556d121d3358b468f0ba
henrikspitu <hspo5master@hotmail.com>
2021-12-16 06:32:40 +0100

    added new OS

--- Kafka-vm/bootstrap.sh
+++ Kafka-vm/bootstrap.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 echo "KEY FILE $1"
-sudo apt-get update
-sudo apt-get install -y  dnsutils
-sudo apt-get install -y curl
+sudo yum update
+sudo yum install -y  dnsutils
+sudo yum install -y curl
 sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt-get update
-sudo apt-get install -y ansible
-sudo apt-get install xauth
-sudo sed -i 's/XKBLAYOUT=.*/XKBLAYOUT="dk"/g' /etc/default/keyboard
+sudo yum update
+sudo yum install -y ansible
+sudo yum install git -y
 sudo useradd -m hspo
-usermod -aG sudo hspo
 sudo mkdir /home/hspo/.ssh
+sudo chown -R hspo:hspo /home/hspo/.ssh
 sudo echo "$1" >> /home/hspo/.ssh/authorized_keys
 sudo echo "$1" >> /root/.ssh/authorized_keys
 sudo chown -R hspo:hspo /home/hspo/.ssh
