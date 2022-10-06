64ebfd81f0eb8b8bb14b67acda962e87b447012f
henrikspitu <hspo5master@hotmail.com>
2022-10-06 11:54:03 +0200

    final install

--- Openstack/bootstrap.sh
+++ Openstack/bootstrap.sh
@@ -3,7 +3,7 @@
 # Update hosts file
 echo "[TASK 1] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
-192.168.86.55 openstack.hspo.com
+192.168.86.56 openstack2.hspo.com
 
 
 EOF
@@ -30,13 +30,6 @@ echo "[TASK 1] Installing apps"
 sudo apt-get update --fix-missing
 sudo apt-get install --reinstall ca-certificates
 
-#apt-get install -y wget
-#apt-get install -y unzip
-
-#sudo yum install -y sipcalc
-#sudo yum install -y w3m
-#sudo yum install -y ipvsadm
-#sudo yum install -y sshpass
 apt-get install -y git
 sudo git config --global user.email "hspo5master@hotmail.com"
 sudo git config --global user.name "henrikspitu"
@@ -46,38 +39,18 @@ echo "[TASK 2] Setup Openstack"
 sudo useradd -s /bin/bash -d /opt/stack -m stack
 sudo chmod +x /opt/stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
-sudo -u stack -i
-git clone https://opendev.org/openstack/devstack
-cd devstack
-echo "ADMIN_PASSWORD=041275" >> local.conf
-echo "DATABASE_PASSWORD=$ADMIN_PASSWORD" >> /opt/stack/devstack/local.conf
-echo "RABBIT_PASSWORD=$ADMIN_PASSWORD" >> /opt/stack/devstack/local.conf
-echo "SERVICE_PASSWORD=$ADMIN_PASSWORD" >> /opt/stack/devstack/local.conf
-echo "HOST_IP=192.168.86.55" >> /opt/stack/devstack/local.conf
-./stack.sh
 
+#sudo -u stack -i
+sudo runuser stack -l -c 'git clone https://opendev.org/openstack/devstack && echo "cloned" ||  echo "clone failed"'
+ls
 
-# install time service so timestamp in logs & metricbeat are correct
-#sudo yum install -y ntp
-#sudo yum install -y ntpdate
-#sudo ntpdate -y ntp.ubuntu.com
-#sudo yum update
-
-
-# Update vagrant user's bashrc file
-#echo "export TERM=xterm" >> /etc/bashrc
-
-# install openstack
-#sudo echo "LANG=en_US.utf-8" >> /etc/environment
-#sudo echo "LC_ALL=en_US.utf-8" >> /etc/environment
-#sudo systemctl disable firewalld
-
-#sudo systemctl stop NetworkManager
-#sudo systemctl disable NetworkManager
-#sudo systemctl start network
-
-# Disable selinux
-#sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+sudo echo "[[local|localrc]]" >> /opt/stack/devstack/local.conf
+sudo echo "ADMIN_PASSWORD=pass4Me" >> /opt/stack/devstack/local.conf
+sudo echo "DATABASE_PASSWORD=\$ADMIN_PASSWORD" >> /opt/stack/devstack/local.conf
+sudo echo "RABBIT_PASSWORD=\$ADMIN_PASSWORD" >> /opt/stack/devstack/local.conf
+sudo echo "SERVICE_PASSWORD=\$ADMIN_PASSWORD" >> /opt/stack/devstack/local.conf
+sudo echo "HOST_IP=$2" >> /opt/stack/devstack/local.conf
+sudo runuser stack -l -c '/opt/stack/devstack/stack.sh'
 
 
 
